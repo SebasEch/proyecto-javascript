22afb4cf557403bba3ec8e437618119c5e392306
Seba Echaide <sebastian.echaide@gmail.com>
2022-08-04 18:54:33 -0300

    elimine codigo innecesario, agregue funcion para numero aleatorio

--- js/app.js
+++ js/app.js
@@ -1,34 +1,19 @@
 // JUEGO PARA ADIVINAR UN NUMERO ALEATORIO GENERADO POR LA APP JS
 
-
-let numeroAleatorio = Math.floor(Math.random() * (10)) + 1
-let selectDificultad = prompt("Seleccione dificultad: \n 1 = facil\n 2= normal \n 3= dificil \n 4= legendario")
-let dificultad = 0;
+// FUNCION QUE DEFINE EL NUMERO CON QUE SE JUGARA 
+const numCom = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+// VARIABLES
+let dificultad = prompt("Seleccione dificultad: \n 1 = facil\n 2= normal \n 3= dificil \n 4= legendario");
 let vidas = 0;
 let aciertos = false;
 
-
-if (selectDificultad == 1) {
-    dificultad = 1
-}
-
-if (selectDificultad == 2) {
-    dificultad = 2
-}
-
-if (selectDificultad == 3) {
-    dificultad = 3
-}
-
-if (selectDificultad == 4) {
-    dificultad = 4
-}
-
-
 if (dificultad == 1) {
+    let numeroAleatorio = numCom(1, 10);
     alert("seleccionaste nivel Facil, deberas adivinar un numero del 1 al 10, tienes vidas infinitas");
     while (aciertos == false) {
-        let numUsuario = prompt("introduce un numero del 1 al 10");
+        let numUsuario = Number(prompt("introduce un numero del 1 al 10"));
         vidas++;
         if (numUsuario == numeroAleatorio) {
             alert("Adivinaste!!! intenta el proximo nivel!")
@@ -36,15 +21,15 @@ if (dificultad == 1) {
             aciertos = true;
             document.write("el numero era " + numeroAleatorio)
         } else {
-            alert("fallaste, llevas " + (vidas) + " intentos")
+            alert("fallaste, llevas " + (vidas) + " vidas")
         }
     }
 }
-
-if (dificultad == 2) {
+else if (dificultad == 2) {
+    let numeroAleatorio = numCom(1, 10)
     alert("seleccionaste nivel Normal, deberas adivinar un numero del 1 al 10, tienes 6 vidas");
     while (aciertos == false && vidas < 6) {
-        let numUsuario = prompt("introduce un numero del 1 al 10");
+        let numUsuario = Number(prompt("introduce un numero del 1 al 10"));
         vidas++;
         if (numUsuario == numeroAleatorio) {
             alert("Adivinaste!!! intenta el proximo nivel!")
@@ -59,12 +44,11 @@ if (dificultad == 2) {
             alert("GAME OVER")
         }
     }
-}
-
-if (dificultad == 3) {
+} else if (dificultad == 3) {
+    let numeroAleatorio = numCom(1, 10)
     alert("seleccionaste nivel Dificil, deberas adivinar un numero del 1 al 10, tienes 3 vidas");
     while (aciertos == false && vidas < 3) {
-        let numUsuario = prompt("introduce un numero del 1 al 10");
+        let numUsuario = Number(prompt("introduce un numero del 1 al 10"));
         vidas++;
         if (numUsuario == numeroAleatorio) {
             alert("Adivinaste!!! intenta el proximo nivel!")
@@ -76,15 +60,14 @@ if (dificultad == 3) {
         }
         if (vidas == 3) {
             document.write("el numero era: " + (numeroAleatorio))
-            alert ("GAME OVER")
+            alert("GAME OVER")
         }
     }
-}
-
-if (dificultad == 4) {
+} else if (dificultad == 4) {
+    let numeroAleatorio = numCom(1, 10)
     alert("seleccionaste nivel Normal, deberas adivinar un numero del 1 al 10, tienes 1 vida");
     while (aciertos == false && vidas < 1) {
-        let numUsuario = prompt("introduce un numero del 1 al 10");
+        let numUsuario = Number(prompt("introduce un numero del 1 al 10"));
         vidas++;
         if (numUsuario == numeroAleatorio) {
             alert("Adivinaste!!! ya eres nivel LEYENDA")
@@ -96,7 +79,7 @@ if (dificultad == 4) {
         }
         if (vidas == 1) {
             document.write("el numero era: " + (numeroAleatorio))
-            alert ("GAME OVER")
+            alert("GAME OVER")
         }
     }
 }
